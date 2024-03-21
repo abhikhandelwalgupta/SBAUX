5fdbcb74b7044f646a4b602f7adcd7ffc060d6db
abhikhandelwalgupta <abhishekkhandelwal1212@gmail.com>
2024-03-21 23:54:05 +0530

    contact form add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,63 +1,83 @@
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable no-unused-vars */
-/* eslint-disable react-refresh/only-export-components */
+
+import HeaderSection from "../components/coman/HeaderSection"
 const Contact = () => {
     return (
         <>
-            <div className="">
-                <div className="w-11/12 h-[40vh] flex justify-center items-center">
-                    <h1 className=" text-4xl text-white ">Contact US</h1>
-                </div>
-                <div className="flex text-black w-full p-20  bg-white">
-                    <div className="bg-[#6C5F5B] rounded-lg p-20 box-content h-[100%] w-[3  0%]">
+
+            <HeaderSection heading={"Contact US"} />
+            <div className="bg-whitesmoke">
+                <div className=" text-black w-11/12 mx-auto md:flex pt-20 pb-20 gap-6 items-start justify-center  ">
+                    <div className="bg-richblack-800 text-whitesmoke rounded-xl md:p-5 md:m-10 box-content  ">
                         <div className="p-5">
-                            <span className="text-2xl ">Chat on us</span>
-                            <p>Our friendly team is here to help. <br />
+                            <span className="md:text-2xl text-xl ">Chat on us</span>
+                            <p className="text-base">Our friendly team is here to help. <br />
                                 info@studynotion.com</p>
                         </div>
                         <div className="p-5">
-                            <span className="text-2xl ">Visit us</span>
-                            <p className="">
+                            <span className="md:text-2xl text-xl ">Visit us</span>
+                            <p className="text-base">
                                 Come and say hello at our office HQ. <br />
                                 Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, <br />
                                 Bangalore-560016
                             </p>
                         </div>
                         <div className="p-5">
-                            <span className="text-2xl ">Call us</span>
-                            <p>
+                            <span className="md:text-2xl text-xl ">Call us</span>
+                            <p className="text-base">
                                 Mon - Fri From 8am to 5pm <br />
                                 +123 456 7869
                             </p>
                         </div>
                     </div>
-                    <div className="p-20 ml-20 border rounded-lg">
-                        <div>
-                            <span className="text-4xl">Got a Idea? We've got the skills. Let's team up</span>
-                            <p className="text-[#95d0a8]">Tell us more about yourself and what you're got in mind.</p>
-                        </div>
-                        <div>
-                            <form className="mt-8 space-y-4">
-                                <p>Name</p>
-                                <input type='text' placeholder='Name'
-                                    className="w-full rounded-md py-3 px-4  text-sm " />
-                                <p>Email</p>
-                                <input type='email' placeholder='Email'
-                                    className="w-full rounded-md py-3 px-4  text-sm " />
-                                <p>Number</p>
-                                <input type='Number' placeholder='Number'
-                                    className="w-full rounded-md py-3 px-4  text-sm " />
-                                <p>Message</p>
-                                <textarea placeholder='Message' rows="6"
-                                    className="w-full rounded-md px-4  text-sm pt-3 "></textarea>
-                                <button type='button'
-                                    className=" bg-blue-500 hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-3 w-full">Send</button>
-                            </form>
+                    <div className="md:p-5 py-4 md:mt-0 mt-4 border rounded-lg ">
+                        <div className="p-4">
+                            <span className="text-4xl">Got a Idea? We have got the skills. Lets team up</span>
+                            <p className="text-[#95d0a8]">Tell us more about yourself and what youre got in mind.</p>
                         </div>
 
+                        <form className=" flex-wrap  bg-whitesmoke w-full md:gap-0 gap-4">
+                            <div className="flex flex-col md:flex-row md:p-4 md:px-0 px-4 md:gap-8 gap-4 ">
+                                <div className="flex flex-col md:w-[50%] w-full">
+                                    <label htmlFor="firstName">First Name</label>
+                                    <input name="firstName" type='text' id="firstName" placeholder='Name'
+                                        className="form-style " />
+
+                                </div>
+                                <div className="flex flex-col md:w-[50%] w-full">
+                                    <label htmlFor="lastName">Last Name</label>
+                                    <input name="lastName" type='text' id="lastName" placeholder='Name'
+                                        className="form-style " />
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col md:flex-row w-full md:p-4 md:px-0 px-4 md:gap-8 md:py-0 py-4 gap-4">
+                                <div className="flex flex-col md:w-[50%] w-full">
+                                    <label htmlFor="email">Email</label>
+                                    <input name="email" type='email' id="email" placeholder='Email' className="form-style" />
+                                </div>
+                                <div className="flex flex-col md:w-[50%] w-full">
+                                    <label htmlFor="mobileNumber">Number</label>
+                                    <input name="mobileNumber" id="mobileNumber" type='Number' placeholder='Number'
+                                        className="form-style " />
+                                </div>
+                            </div>
+                            <div className="flex flex-col w-full md:p-4 md:px-0 px-4  ">
+                                <label htmlFor="message">Message</label>
+                                <textarea id="message" placeholder='Message' rows="6"
+                                    className="form-style"></textarea>
+                            </div>
+                            <div className="flex w-full md:p-4 md:px-0 px-4 md:py-0 py-4">
+                                <button type='button'
+                                    className=" bg-richblue-800 text-white  font-semibold rounded-md text-sm px-4 py-3 w-full">Send</button>
+
+                            </div>
+                        </form>
                     </div>
-                </div>
+
+                </div >
+
             </div>
+
         </>
 
     )
